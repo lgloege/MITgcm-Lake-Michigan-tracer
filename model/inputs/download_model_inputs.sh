53c1b5202b4aec15527f0d1ad0623b9868e2edaf
Luke Gloege <gloege@ldeo.columbia.edu>
2018-09-08 19:06:04 -0400

    added download_model_inputs.sh

--- model/inputs/download_model_inputs.sh
+++ model/inputs/download_model_inputs.sh
@@ -1,24 +1,43 @@
 #!/usr/bin/env bash
-### ==========================================
-### download_model_inputs.sh <YEAR> <DIR>
-###     This script downloads binary
-### model input for a specified year
-### stored on figshare to a specified directory
-###    inputs :
-###  year = 2007, 2008, 2009, or 2010
-###  dir = directory to move inputs to
-###        if no dir is defined then
-###        inputs are moved to ./inputs
+### ===================================================================
+### download_model_inputs.sh
+###     Downloads binary inputs for all four years from figshare
+###     four directries are created when this script completes:
+###     inputs_2007, inputs_2008, inputs_2009, inputs_2010
 ###
-###     example:
-### ./downlooad_model_inputs.sh 2007
-### (this will move inputs for the year 2007
-### to a directory named ./inputs)
-### =============================================
-
-### This is just good practice
-### prints errors if variables are not set
+###  Usage:  ./downlooad_model_inputs.sh
+###
+###  output:
+###    This will output three folders:
+###    inputs_2007, inputs_2008, inputs_2009, inputs_2010
+### ===================================================================
 set -o errexit
 set -o nounset
 
-### NOT READY YET
+### Download and unzip 2007 model inputs
+echo "Downloading inputs_2007.zip..."
+wget https://ndownloader.figshare.com/files/12990956
+echo "Unzipping inputs_2007.zip"
+unzip 12990956
+rm 12990956
+
+### Download and unzip 2008 model inputs
+echo "Downloading inputs_2008.zip..."
+wget https://ndownloader.figshare.com/files/12991247
+echo "Unzipping inputs_2008.zip"
+unzip 12991247
+rm 12991247
+
+### Download and unzip 2009 model inputs
+echo "Downloading inputs_2009.zip..."
+wget https://ndownloader.figshare.com/files/12991235
+echo "Unzipping inputs_2009.zip"
+unzip 12991235
+rm 12991235
+
+### Download and unzip 2010 model inputs
+echo "Downloading inputs_2010.zip..."
+wget https://ndownloader.figshare.com/files/12991241
+echo "Unzipping inputs_2010.zip"
+unzip 12991241
+rm 12991241
