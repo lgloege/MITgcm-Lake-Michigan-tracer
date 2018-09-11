cd4136774719ae61584e62f0831e2a9bf9ce4dd8
Luke Gloege <gloege@ldeo.columbia.edu>
2018-09-11 10:34:42 -0400

    updated build_MITgcm.bash

--- model/build_MITgcm.bash
+++ model/build_MITgcm.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 #======================================================================
-# MITgcm build script
+# MITgcm build script setup to build on Habanero at Columbia Uni.
 #
 # rootdir = path to model source directory
 # optfile = path to system optfile
 # mods    = directory where you modified source files
+#
+# L. Gloege 2018
 #======================================================================
 set -o errexit
 set -o nounset
@@ -14,10 +16,9 @@ set -o nounset
 ### vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 rootdir="/rigel/ocpbgc/users/luke/MITgcm-lm/source"
 optfile="${rootdir}/tools/build_options/linux_ia64_ifort_habanero"
-mods='/rigel/ocpbgc/users/luke/MITgcm-lm/code/LM_11_rivers'
+mods="/rigel/ocpbgc/users/luke/MITgcm-lm/code"
 
 ### Load modules
-#module load slurm
 module load intel-parallel-studio/2017
 module load netcdf-fortran/4.4.4
 module load netcdf/gcc/64/4.4.0
